7d8197af6d2dee27e5077a34a6f9dd2b90792ec7
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-10-20 10:03:42 +0300

    Handling Errors

--- _printf.c
+++ _printf.c
@@ -1,54 +1,5 @@
 #include "main.h"
 /**
-*get_func - produces output according to format
-*
-*@format: string to be printed
-*@ap: argument parameters
-*
-*Return: number of charaters printed else 0
-*/
-int get_func(const char *format, va_list ap)
-{
-	int i, j, counter = 0, size = 0;
-
-	selector funcs[] = {
-		{"c", print_char}, {"s", print_string},
-		{"d", print_int}, {"i", print_int},
-		{"b", decimaltobinary}
-	};
-	for (i = 0; *(format + i) != '\0'; i++)
-	{
-		j = 0;
-		if (*(format + i) == '%')
-		{
-			i++;
-			if (*(format + i) == '\0')
-				return (-1);
-			while (j < 5 && (*(format + i) != *(funcs[j].ident)))
-				j++;
-
-			if (j < 5)
-				size = funcs[j].f(ap);
-			else if (*(format + i) == '%')
-			{
-				write(1, (format + i), 1);
-				size += 1;
-			}
-			else
-			{
-				_putchar('%'), _putchar(*(format + i));
-				size += 2;
-			}
-		}
-		else
-		{
-			write(1, (format + i), 1);
-			counter++;
-		}
-	}
-	return (counter + size);
-}
-/**
  * _printf - function that produces output according to a format.
  * @format: string to be printed in a specifc format
  *
@@ -56,21 +7,42 @@ int get_func(const char *format, va_list ap)
  */
 int _printf(const char *format, ...)
 {
-	int i, size;
+	unsigned int i = 0, size = 0;
 	va_list ap;
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
-	for (i = 0; *(format + i) != '\0'; i++)
-		;
-	if (i <= 0)
-		return (0);
-
 	va_start(ap, format);
 
-	size = get_func(format, ap);
-
-	va_end(ap);
-
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				size++;
+				i++;
+			}
+			else if (get_func(format[i + 1]) != NULL)
+			{
+				size += (get_func(format[i + 1]))(ap);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				size++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			size++;
+		}
+	}
 	return (size);
+	va_end(ap);
 }

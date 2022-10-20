8db62f720b39e5d1bc5ed273878319457bb65a05
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-10-20 17:30:22 +0300

    Handling p, x, X, hexadecimal, power specifiers

--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int (*get_func(const char format))(va_list);
+int print_hex(unsigned int n, unsigned int c);
 
 /**
  * struct select - defines a structure for symbols and functions
@@ -27,6 +28,9 @@ int print_int(va_list ap);
 int print_int(va_list ap);
 int decimaltobinary(va_list ap);
 int print_unsigned_int(va_list ap);
+int print_address(va_list ap);
 int print_R(va_list ap);
+int print_X(va_list ap);
+int print_x(va_list ap);
 
 #endif /*MAIN_h*/
